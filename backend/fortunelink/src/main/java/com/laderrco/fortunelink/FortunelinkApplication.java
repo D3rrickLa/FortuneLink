9b29e4e75f40974e30093d39dca77dd8af39a644
Derrick <lader@sheridancollege.ca>
2025-06-15 20:21:39 -0400

    Created Spring Backend

---
+++ backend/fortunelink/src/main/java/com/laderrco/fortunelink/FortunelinkApplication.java
@@ -0,0 +1,13 @@
+package com.laderrco.fortunelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortunelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortunelinkApplication.class, args);
+	}
+
+}
---
+++ backend/fortunelink/src/test/java/com/laderrco/fortunelink/FortunelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laderrco.fortunelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortunelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
