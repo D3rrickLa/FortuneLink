dc0093dc6c4a524e5ba744562c1c484e6470f8c0
D3rrick La <lader@sheridancollege.ca>
2026-02-13 21:26:32 -0500

    update

--- backend/fortunelink/src/main/java/com/laderrco/fortunelink/portfolio_management/application/services/PortfolioService.java
+++ backend/fortunelink/src/main/java/com/laderrco/fortunelink/portfolio_management/application/services/PortfolioService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import com.laderrco.fortunelink.portfolio_management.application.commands.AddAccountCommand;
 import com.laderrco.fortunelink.portfolio_management.application.commands.CreatePortfolioCommand;
 import com.laderrco.fortunelink.portfolio_management.application.queries.views.PortfolioView;
+import com.laderrco.fortunelink.portfolio_management.application.validators.CommandValidator;
+import com.laderrco.fortunelink.portfolio_management.application.validators.ValidationResult;
 import com.laderrco.fortunelink.portfolio_management.domain.repositories.PortfolioRepository;
 
 import jakarta.transaction.Transactional;
@@ -17,24 +19,33 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PortfolioService {
     private final PortfolioRepository portfolioRepository;
+    private final CommandValidator validator;
 
     public PortfolioView createPortfolio(CreatePortfolioCommand command) {
+        ValidationResult result = validator.validate(command);
+        if (!result.isValid()) {
+            throw new InvalidCommandException("Invalid create portfolio command", result.errors());
+        }
         long currentCount = portfolioRepository.countByUserId(command.userId());
         int maxProfileAllowed = 1;
         if (currentCount >= maxProfileAllowed) {
             // throws
         }
 
-        
     }
 
-    public void updatePortfolio() {}
-    
-    public void deletePortfolio() {}
-    
-    public void createAccount(AddAccountCommand command) {}
+    public void updatePortfolio() {
+    }
 
-    public void updateAccount() {}
+    public void deletePortfolio() {
+    }
 
-    public void deleteAccount() {}
+    public void createAccount(AddAccountCommand command) {
+    }
+
+    public void updateAccount() {
+    }
+
+    public void deleteAccount() {
+    }
 }
