1b4440730de20627a675c6f89c9786e146b5b562
D3rrick La <lader@sheridancollege.ca>
2026-03-26 22:45:06 -0400

    Removed Exceptions not in use

--- backend/fortunelink/src/main/java/com/laderrco/fortunelink/portfolio/application/exceptions/PortfolioAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.laderrco.fortunelink.portfolio.application.exceptions;
-
-public class PortfolioAlreadyExistsException extends RuntimeException {
-
-  public PortfolioAlreadyExistsException(String string) {
-    super(string);
-  }
-
-}
