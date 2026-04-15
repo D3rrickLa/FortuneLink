f51df3fc16bbace3caf27dccb414774cdd5250c2
D3rrick La <lader@sheridancollege.ca>
2026-04-15 19:47:35 -0400

    Fixing workflow

--- backend/fortunelink/src/main/java/com/laderrco/fortunelink/portfolio/infrastructure/config/redis/RedissonConfig.java
+++ backend/fortunelink/src/main/java/com/laderrco/fortunelink/portfolio/infrastructure/config/redis/RedissonConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class RedissonConfig {
@@ -32,7 +33,7 @@ public class RedissonConfig {
         .setConnectionPoolSize(10).setTimeout(2000).setRetryAttempts(3)
         .setRetryDelay(new EqualJitterDelay(Duration.ofMillis(500), Duration.ofMillis(1000)));
 
-    if (!password.isBlank()) {
+    if (password != null && !password.trim().isEmpty()) {
       config.setPassword(password);
     }
 
